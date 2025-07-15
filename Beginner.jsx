2f7d1cb4e1895c911ecfecc3b89b0540e2891d6c
LoganpsJOGA <lshalty202@gmail.com>
2025-07-15 13:19:49 -0600

    Add files via upload

---
+++ Beginner.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Beginner() {
+  return <div className="p-4 text-center text-xl font-bold">Beginner Page</div>;
+}
---
+++ Chat.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Chat() {
+  return <div className="p-4 text-center text-xl font-bold">Chat Page</div>;
+}
